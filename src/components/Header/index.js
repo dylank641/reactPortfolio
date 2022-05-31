5b48099aa8064b94a1116833e80cd408ede10d1a
James Killelea <dylan.killelea@yahoo.com>
2022-05-31 11:42:41 -0500

    nav

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header(props) {
+
+    const { currentPage, setCurrentPage } = props;
+
+    return (
+        <header>
+            <div className="container flex-row justify-space-between align-center py-3">
+                <h1 id="name-header" className="page-title text-secondary bg-dark py-2 px-3">Dylan Killelea</h1>
+                <Nav
+                    currentPage={currentPage}
+                    setCurrentPage={setCurrentPage}
+                ></Nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
