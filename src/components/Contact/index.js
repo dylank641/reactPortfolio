75c5eaa68f12dd88b40dcda19a495da1caa4be73
James Killelea <dylan.killelea@yahoo.com>
2022-05-30 23:53:57 -0500

    working on components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,34 +3,34 @@ import React, { useState } from 'react';
 function Contact() {
 
     const [formPack, setFormPack] = useState({ name: '', email: '', message: '' });
-    const [ errMsg, seterrMsg] = useState('');
+    const [ errorMsg, setErrorMsg] = useState('');
     const { name, email, message } = formPack;
 
-    const handleSubmit = (event) => {
+    const submit = (event) => {
         event.preventDefault();
-        if (!errMsg) {
+        if (!errorMsg) {
             console.log(formPack);
         }
     }
 
-    const handleChange = (event) => {
+    const check = (event) => {
 
         //checks name
         if (event.target.name === 'name') {
             const targetValue = event.target.value.trim();
             if (targetValue.length < 1) {
-                seterrMsg("Name cannot be empty!");
+                setErrorMsg("Please enter a name.");
             } else {
-                seterrMsg("");
+                setErrorMsg("");
             }
         }
 
         //checks email
         if (event.target.name === 'email') {
             if (!emailIsValid(event.target.value)) {
-                seterrMsg("Email is not valid!");
+                setErrorMsg("E-mail is not valid.");
             } else {
-                seterrMsg("");
+                setErrorMsg("");
             }
         }
 
@@ -38,14 +38,56 @@ function Contact() {
         if (event.target.name === 'message') {
             const targetValue = event.target.value.trim();
             if (targetValue.length < 1) {
-                seterrMsg("Message is required!");
+                setErrorMsg("Message required.");
             } else {
-                seterrMsg("");
+                setErrorMsg("");
             }
         }
 
       
-        if (!errMsg) {
+        if (!errorMsg) {
             setFormPack({...FormData, [event.target.name]: event.target.value });
         }
     }
+
+    return (
+        <section className='my-3' id="contact">
+            <h2 id="contact-header" className='text-dark bg-primary p-2 display-inline-block'>Contact</h2>
+            <div>
+                <form onSubmit={submit}>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td><label htmlFor="name">Name:</label></td>
+                                <td><input type="text" name="name" defaultValue={name} onBlur={check} /></td>
+                                <td rowSpan={3} className="text-center">The form to the left is not finished yet so for now just email me at <a href="mailto:patrickaregan@gmail.com" target={'_blank'}>patrickaregan@gmail.com</a> or call me at 512-914-2010.</td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="email">Email:</label></td>
+                                <td><input type="email" name="email" defaultValue={email} onBlur={check} /></td>
+                            </tr>
+                            <tr>
+                                <td><label htmlFor="message">Message:</label></td>
+                                <td><textarea name="message" rows="5" defaultValue={message} onBlur={check}></textarea></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                {errorMsg && (
+                                    <p className="error-message">{errorMsg}</p>
+                                )}
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td><button type="submit">Submit</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </form>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
