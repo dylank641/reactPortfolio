e2401cd4732aa53123b3c01b16a483f9f6aeb8f2
James Killelea <dylan.killelea@yahoo.com>
2022-05-30 23:59:58 -0500

    footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+
+    // get the current year
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+
+    // my profile URLs
+    let gitURL = "https://github.com/dylank641";
+    let linkURL = "https://www.linkedin.com/in/james-killelea-25a60a19a/";
+    
+
